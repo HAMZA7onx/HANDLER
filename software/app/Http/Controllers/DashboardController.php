170aca3de3a8171307fd81c3d126ee68927d244d
MESKI HAMZA <88968389+HAMZA7onx@users.noreply.github.com>
2024-01-23 18:14:33 +0100

    search bar implementation

--- software/app/Http/Controllers/DashboardController.php
+++ software/app/Http/Controllers/DashboardController.php
@@ -10,7 +10,14 @@ use App\Models\Idea;
 class DashboardController extends Controller
 {
     public function index() {
-        $ideas = Idea::orderBy('created_at', 'DESC')->paginate(3);
+        $ideas = Idea::orderBy('created_at', 'DESC');
+
+        if(request()->has('search')) {
+            $ideas = $ideas->where('content', 'like', '%' . request()->get('search','') . '%');
+        }
+
+        $ideas = $ideas->paginate(3);
+
         return view('dashboard', compact('ideas'));
     }
 }
