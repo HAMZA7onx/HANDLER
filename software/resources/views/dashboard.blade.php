65d40057fc9be40968a2b422ed090244b8134f0a
MESKI HAMZA <88968389+HAMZA7onx@users.noreply.github.com>
2024-01-23 15:08:00 +0100

    Organizing dashboard tags

--- software/resources/views/dashboard.blade.php
+++ software/resources/views/dashboard.blade.php
@@ -37,27 +37,9 @@
             </div>
         </div>
         <div class="col-6">
-            <div class="alert alert-success alert-dismissible fade show" role="alert">
-                Idea created Successfully
-                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-            </div>
-            <h4> Share yours ideas </h4>
-            <form action="{{ route('ideas.store') }}" method="post">
-                @csrf
-                <div class="row">
-                    <div class="mb-3">
-                        <textarea name="content" class="form-control" id="idea" rows="3"></textarea>
-                        <div class="text-danger">
-                            @error('content')
-                                {{ $message }}
-                            @enderror
-                        </div>
-                    </div>
-                    <div class="">
-                        <button type="submit" class="btn btn-dark"> Share </button>
-                    </div>
-                </div>
-            </form>
+            @include('shared.success-message')
+
+            @include('shared.submit-idea')
 
             <hr>
             <div class="mt-3">
