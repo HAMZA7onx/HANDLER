28b241d2247c30af0aea85816d476f77c7d5f85b
MESKI HAMZA <88968389+HAMZA7onx@users.noreply.github.com>
2024-01-23 13:21:29 +0100

    config('app.name')

--- software/resources/views/layout/layout.blade.php
+++ software/resources/views/layout/layout.blade.php
@@ -1,6 +1,8 @@
 @include('layout.header')
 @include('layout.nav-bar')
 
-@yield('content')
+<div class="container py-4">
+    @yield('content')
+</div>
 
 @include('layout.footer')
--- software/resources/views/layout/nav-bar.blade.php
+++ software/resources/views/layout/nav-bar.blade.php
@@ -2,7 +2,7 @@
 <nav class="navbar navbar-expand-lg bg-dark border-bottom border-bottom-dark ticky-top bg-body-tertiary"
      data-bs-theme="dark">
     <div class="container">
-        <a class="navbar-brand fw-light" href="/"><span class="fas fa-brain me-1"> </span>Ideas</a>
+        <a class="navbar-brand fw-light" href="/"><span class="fas fa-brain me-1"> </span>{{ config('app.name') }}</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -22,3 +22,4 @@
         </div>
     </div>
 </nav>
+
