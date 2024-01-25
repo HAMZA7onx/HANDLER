7a8d70516b76655749079d26c1982fc90737c5f2
MESKI HAMZA <88968389+HAMZA7onx@users.noreply.github.com>
2024-01-25 01:10:58 +0100

    Follow controller setup

--- software/app/Http/Controllers/FollowerController.php
+++ software/app/Http/Controllers/FollowerController.php
@@ -1,10 +1,16 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class FollowerController extends Controller
 {
-    //
+    public function follow() {
+
+    }
+
+    public function unfollow() {
+        
+    }
 }
--- software/app/Http/Controllers/IdeaController.php
+++ software/app/Http/Controllers/IdeaController.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use App\Models\Idea;
 use Illuminate\Http\Request;
 
